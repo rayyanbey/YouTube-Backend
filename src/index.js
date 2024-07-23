e58ec790ce916e0c58b70087b623a87c21d996a0
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-07-23 12:52:32 +0500

    connection to Mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import connectDB from './database/index.js'
+import dotenv from "dotenv"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+dotenv.config({
+    path: '../public/.env'
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+connectDB() //DATABASE CONNECTION
